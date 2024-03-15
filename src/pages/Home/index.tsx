b0b0bf5281960771a2773c42e1f007c7e68341a9
wylgberthy08 <berthymendes04@gmail.com>
2024-03-15 00:19:30 -0400

    feat(Home):Firebase items list

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,54 @@
+import { useState, useEffect } from "react";
 import { Container } from "../../components/container";
 
+import { collection, query, getDocs, orderBy } from "firebase/firestore";
+import { db } from "../../services/firebaseConnection";
+import { Link } from "react-router-dom";
+
+interface CarProps {
+  id: string;
+  name: string;
+  year: string;
+  uid: string;
+  price: string | number;
+  city: string;
+  km: string;
+  images: CarImageProps[];
+}
+interface CarImageProps {
+  name: string;
+  uid: string;
+  url: string;
+}
+
 export function Home() {
+  const [cars, setCars] = useState<CarProps[]>([]);
+
+  useEffect(() => {
+    //busca carros
+    function loadCars() {
+      const carsRef = collection(db, "cars");
+      const queryRef = query(carsRef, orderBy("created", "desc"));
+      getDocs(queryRef).then((snapshot) => {
+        const listCars = [] as CarProps[];
+
+        snapshot.forEach((doc) => {
+          listCars.push({
+            id: doc.id,
+            name: doc.data().name,
+            year: doc.data().year,
+            km: doc.data().km,
+            city: doc.data().city,
+            price: doc.data().price,
+            images: doc.data().images,
+            uid: doc.data().uid,
+          });
+        });
+        setCars(listCars);
+      });
+    }
+    loadCars();
+  }, []);
   return (
     <Container>
       <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
@@ -17,27 +65,29 @@ export function Home() {
       </h1>
 
       <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-        <section className="w-full bg-white rounded-lg">
-          <img
-            className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
-            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202310/20231014/fiat-argo-1.0-firefly-flex-drive-manual-wmimagem12343705972.jpg?s=fill&w=1920&h=1440&q=75"
-            alt="carro"
-          />
-          <p className="font-bold mt-1 mb-2 px-2">BMW 3201</p>
+        {cars.map((car) => (
+          <Link key={car.id} to={`/car/${car.id}`}>
+            <section className="w-full bg-white rounded-lg">
+              <img
+                className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+                src={car.images[0].url}
+                alt="carro"
+              />
+              <p className="font-bold mt-1 mb-2 px-2">{car.name}</p>
 
-          <div className="flex flex-col px-2">
-            <span className="text-zinc-700 mb-6">
-              Ano 2016/2016 | 23.000 KM
-            </span>
-            <strong className="text-black font-medium text-x1">
-              R$ 190.000
-            </strong>
-          </div>
-          <div className="w-full h-px bg-slate-700 my-2"></div>
-          <div className="px-2 pb-2">
-            <span className="text-zinc-700">Campo grande-MS</span>
-          </div>
-        </section>
+              <div className="flex flex-col px-2">
+                <span className="text-zinc-700 mb-6">{car.year}</span>
+                <strong className="text-black font-medium text-x1">
+                  R$ {car.price}
+                </strong>
+              </div>
+              <div className="w-full h-px bg-slate-700 my-2"></div>
+              <div className="px-2 pb-2">
+                <span className="text-zinc-700">{car.city}</span>
+              </div>
+            </section>
+          </Link>
+        ))}
       </main>
     </Container>
   );
